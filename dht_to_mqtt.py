ad8c983155b1cf5c34f867a1cdd201e51bb02ccd
Demo User <debian@beaglebone.localdomain>
2018-06-16 03:25:33 +0000

    fix up connect

--- dht_to_mqtt.py
+++ dht_to_mqtt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # required libraries
+import datetime
 import json
 import time
 import Adafruit_DHT
@@ -26,9 +27,11 @@ SENSOR_MAPPING = {
     '2302': Adafruit_DHT.AM2302
 }
 
+# connection status
+connected = False
+
 def read_sensor(sensor, pin):
-    print "read sensor"
-    # read_retry will 
+    # read_retry will try multiple times up to 15 seconds
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     return humidity, temperature
 
@@ -36,27 +39,44 @@ def publish_reading(client, endpoint, topic, id, location, humidity, temperature
     print "publish"
     # setup sensor payload
     payload = {
+        'timestampe': int(time.mktime((datetime.datetime.utcnow()).timetuple())),
         'location': location,
         'humidity': humidity,
         'temperature': temperature
     }
     payload_json = json.dumps(payload)
-
-    # create MQTT client
-    client.connect(endpoint['host'], endpoint['port'])
     client.publish(topic, payload_json)
     return 0
 
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print "connected"
+        global connected
+        connected = True
+    else:
+        print "error connecting"
+
+def on_publish(client, userdata, mid):
+    print "published message: " + str(mid)
+
 def main():
-    # validate config
+    global connected
 
     # setup mqtt client
     client = mqtt.Client(SENSOR_ID)
     client.username_pw_set(MQTT_END_POINT['username'], MQTT_END_POINT['password'])
+    client.on_publish = on_publish
+    client.on_connect = on_connect
+    print "Connecting to: " + MQTT_END_POINT['host'] + ", port: " + str(MQTT_END_POINT['port'])
+    client.connect(MQTT_END_POINT['host'], MQTT_END_POINT['port'])
+    client.loop_start()
+
+    # wait until client is connected
+    while connected == False:
+        time.sleep(0.1)
     
     # execute loop
     while True:
-        print "in loop"
         humidity, temperature = read_sensor(SENSOR_MAPPING[SENSOR_TYPE], SENSOR_PIN)
         if humidity is None and temperature is None:
             print "Failed to get reading."
